829b43dcba53cf23e126495198382ff676bf7778
Dario Guzik <dario.guzik@mercadolibre.com>
2012-08-29 14:24:30 -0300

    first commit. hasmany not working

---
+++ grails-app/controllers/chainedstory/ParagraphsController.groovy
@@ -0,0 +1,35 @@
+package chainedstory
+
+class ParagraphsController {
+
+    def index() { 
+		render " saraza"
+		}
+	
+	def write () {
+		//write a new paragraph
+		def parent = params.parent_paragraph
+		def theParagraph = new Paragraph()
+		theParagraph.leftSteps = 10
+		
+		if (parent) {
+			theParagraph.parent = parent
+			theParagraph.leftSteps = (Paragraph.findById(parent)?.leftSteps?:10)-1
+		}
+		theParagraph.author = params.author
+		theParagraph.content = params.content
+		
+		println theParagraph.save()
+		println "objeto: ${theParagraph.properties.toString()}"
+		render "ok "
+	}
+	
+	def list () {
+		def salida = []
+		Paragraph.list().each {
+			println it.properties.toString()
+			salida << "${it.id} -> Author: ${it.author}, Parent: ${it.parent}, content: ${it.content}"
+		}
+		render salida
+	}
+}
