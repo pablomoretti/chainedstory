71a28e1580ff941f996392a75334b9e921e7e165
Dario Guzik <dario.guzik@mercadolibre.com>
2012-09-06 01:19:11 -0300

    refactor

--- grails-app/controllers/chainedstory/StoriesController.groovy
+++ grails-app/controllers/chainedstory/StoriesController.groovy
@@ -14,21 +14,30 @@ class StoriesController {
 
 	def index() {
 		println "want to get story ${params.id}"
-		def story = storiesService.getStory(params.id)
+		def story = storiesService.getCompleteStory(params.id)
 		println story
 		return [story:story]
 	}
 
 	def add(){
-
+		start()
 		println "save ${params.paragraph}"
 		println "id requested ${params.paragraph_id}"
 
 		if (params.paragraph_id == null) {
-			def newStory = storiesService.addStory(author:request.getAttribute("facebook").user_id, content:params.paragraph,oauthToken:request.getAttribute("facebook").oauth_token)
+			def newStory = storiesService.addNewStory(
+				author:request.getAttribute("facebook").userId, 
+				content:params.paragraph,
+				oauthToken:request.getAttribute("facebook").oauth_token,
+				title: params.title,
+				steps: params.steps)
 			redirect(action:"congrats", params:[id:newStory])
 		} else {
-			def newParagraph = storiesService.addParagraph(paragraph:params.paragraph_id,author:request.getAttribute("facebook").user_id, content:params.paragraph,oauthToken:request.getAttribute("facebook").oauth_token)
+			def newParagraph = storiesService.addNewParagraph(
+				paragraph:params.paragraph_id,
+				author:request.getAttribute("facebook").userId, 
+				content:params.paragraph,
+				oauthToken:request.getAttribute("facebook").oauth_token)
 			redirect(action:"congrats", params:[id:newParagraph])
 		}
 	}
