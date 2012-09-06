dcec96b8e83eea778f109a8187e5cdb22c2d8d2d
Dario Guzik <dario.guzik@mercadolibre.com>
2012-09-06 02:00:01 -0300

    JSON marshallers

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -2,15 +2,30 @@ import java.util.List;
 
 import chainedstory.Paragraph;
 import grails.converters.JSON
+import chainedstory.Story;
 
 class BootStrap {
 
     def init = { servletContext ->
-    /*    JSON.registerObjectMarshaller(Story) {
+        JSON.registerObjectMarshaller(Story) {
             def returnArray = [:]
             returnArray['title'] = it.title
+            returnArray['id'] = it.id
+            returnArray['category'] = it.category
+            returnArray['created'] = it.created
+            returnArray['status'] = it.status
             return returnArray
-        }*/
+        }
+        JSON.registerObjectMarshaller(Paragraph) {
+            def returnArray = [:]
+            returnArray['content'] = it.content
+            returnArray['id'] = it.id
+            returnArray['author_id'] = it.authorId
+            returnArray['author_name'] = it.authorName
+            returnArray['created'] = it.created
+            returnArray['facebook_id'] = it.facebookId
+            return returnArray
+        }
     }
     def destroy = {
     }
