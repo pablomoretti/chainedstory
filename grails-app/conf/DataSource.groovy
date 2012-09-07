f4b1131c803686654cf3a71d4b9a202863f0fdd7
Pablo Moretti <pablomoretti@gmail.com>
2012-09-06 23:06:22 -0300

    mysql

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,43 +1,49 @@
-dataSource {
-    pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
-}
 hibernate {
-    cache.use_second_level_cache = true
-    cache.use_query_cache = true
-    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
+	cache.use_second_level_cache = false
+	cache.use_query_cache = false
+	cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
 }
 // environment specific settings
 environments {
-    development {
-        dataSource {
-            dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE"
-        }
-    }
-    test {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE"
-        }
-    }
-    production {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE"
-            pooled = true
-            properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
-            }
-        }
-    }
+	development {
+		dataSource {
+			pooled = true
+			driverClassName = "org.h2.Driver"
+			username = "sa"
+			password = ""
+			dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+			url = "jdbc:h2:mem:devDb;MVCC=TRUE"
+		}
+	}
+	test {
+		dataSource {
+			pooled = true
+			driverClassName = "org.h2.Driver"
+			username = "sa"
+			password = ""
+			dbCreate = "create-drop"
+			url = "jdbc:h2:mem:testDb;MVCC=TRUE"
+		}
+	}
+	production {
+		dataSource {
+			dbCreate = "update"
+			driverClassName = "com.mysql.jdbc.Driver"
+			dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
+			username = "b81fcb0e1b7f81"
+			password = "8684f768"
+			url = "jdbc:mysql://us-cdbr-east.cleardb.com/heroku_35d817616b4f041?reconnect=true&useUnicode=yes&characterEncoding=UTF-8"
+			pooled = true
+			properties {
+				maxActive = 5
+				minEvictableIdleTimeMillis=1800000
+				timeBetweenEvictionRunsMillis=1800000
+				numTestsPerEvictionRun=3
+				testOnBorrow=true
+				testWhileIdle=true
+				testOnReturn=true
+				validationQuery="SELECT 1"
+			}
+		}
+	}
 }
